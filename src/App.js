7d7dc63c4c4adefa97e2fefd80860b9c7bd922f0
sehaj anand <sehajanand.321@gmail.com>
2021-12-09 18:45:08 +0530

    third notes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import Footer from "./Components/Footer.js";
 import Navbar from "./Components/Navbar.js";
 import Textarea from "./Components/Textarea.js";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Notes from "./Components/Notes.js";
+import Notes from "./Components/Notes.js";
 
 function App() {
+  function showTextarea(){
+    if(window.location.pathname==="/"){
+      return <Textarea/>
+    }
+  }
+  function showNotes(){
+    if(window.location.pathname==="/notes"){
+      return<Notes/>
+    }
+  }
   return (
     <>
       {/* <Router> */}
@@ -13,7 +23,9 @@ function App() {
           <Navbar />
         </header>
         <main>
-          <Textarea/>
+          {showTextarea}
+          {showNotes}
+          {/* <Textarea/> */}
           {/* <Routes> */}
             {/* <Route path="/" element={<Textarea />} />
             <Route path="/notes" element={<Notes />} />
